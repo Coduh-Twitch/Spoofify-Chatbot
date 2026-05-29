e38723758d1911b69bb33a1c48e1425d02f4cc0d
ducky <asqrybot@gmail.com>
2026-05-29 15:44:19 -0400

    fix: typo

--- src/commands/spoofifyCommands.ts
+++ src/commands/spoofifyCommands.ts
@@ -5,7 +5,7 @@ import {getQueue} from "../lib/spoofify";
 const spoofifyCommand = async (hasAuthority: boolean, channel: string, user: string, text: string, msg: ChatMessage, args: string[]) => {
    
 
-    await reply(channel, `${emoji} @${process.env.BOT_USER_NAME}'s Spotify features are powered by Spoofify! Create by @duckyyylive, Spoofify is a Now Playing Overlay and Queue Management Twitch bot. Learn more about Spoofify here: https://spoofify.live`, msg.id)
+    await reply(channel, `${emoji} @${process.env.BOT_USER_NAME}'s Spotify features are powered by Spoofify! Created by @duckyyylive, Spoofify is a Now Playing Overlay and Queue Management Twitch bot. Learn more about Spoofify here: https://spoofify.live`, msg.id)
 }
 
 export default spoofifyCommand;
